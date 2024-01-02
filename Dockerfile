fb5dfb838ed03112d4dc2d883ff85af672990551
rbott <rbott@users.noreply.github.com>
2024-01-02 20:11:42 +0000

    deploy: 3fcd0bd9868b102c1c5679dad69a72e9e0c96719

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM node:21
-
-# Set the ports
-EXPOSE 3000
-EXPOSE 3001
-
-CMD [ "/bin/bash" ]
--- _11ty/transforms/minifyJSON.js
+++
@@ -1,9 +0,0 @@
-const jsonminify = require('jsonminify');
-const { IS_PRODUCTION } = require('../constants');
-
-module.exports = (content, outputPath) => {
-  if (IS_PRODUCTION && outputPath && outputPath.endsWith('.json')) {
-    return jsonminify(content);
-  }
-  return content;
-};
